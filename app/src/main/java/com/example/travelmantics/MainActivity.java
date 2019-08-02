031773e70eeecda9ac632540c0cfd641cde40d41
--list <stann09@gmail.com>
2019-08-02 12:02:33 +0300

    add save menu

--- app/src/main/java/com/example/travelmantics/MainActivity.java
+++ app/src/main/java/com/example/travelmantics/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.travelmantics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //create instance of menuInflater object
+        MenuInflater inflater = getMenuInflater();
+        //pass it the XML resource we want to inflate
+        inflater.inflate(R.menu.save_menu,menu);
+
+        return true;
     }
 }
